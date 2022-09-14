405ca60dc23b12262a73a513a3226f0c3659d504
alessandroscoelho <alessandroscoelho@gmail.com>
2022-09-14 17:19:25 -0300

    implementacao repository 2

--- src/main/java/com/api/springboot/repository/CategoriaRepository.java
+++ src/main/java/com/api/springboot/repository/CategoriaRepository.java
@@ -1,8 +1,10 @@
 package com.api.springboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.api.springboot.domain.Categoria;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
 
 }
