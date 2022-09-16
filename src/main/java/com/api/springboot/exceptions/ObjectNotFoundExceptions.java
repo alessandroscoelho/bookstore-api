60536b4080559d852b98c29860ed3ef79218a2a6
alessandroscoelho <alessandroscoelho@gmail.com>
2022-09-16 15:04:51 -0300

    findById para categoria

---
+++ src/main/java/com/api/springboot/exceptions/ObjectNotFoundExceptions.java
@@ -0,0 +1,17 @@
+package com.api.springboot.exceptions;
+
+
+public class ObjectNotFoundExceptions extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundExceptions(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ObjectNotFoundExceptions(String message) {
+		super(message);
+	}
+
+	
+}
