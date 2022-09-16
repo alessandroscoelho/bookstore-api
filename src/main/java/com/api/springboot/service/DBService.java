0cf30e7a2162d1ff8c6783d4320f4cd6b44e8b60
alessandroscoelho <alessandroscoelho@gmail.com>
2022-09-16 14:32:48 -0300

    implementacao do perfil dev

--- src/main/java/com/api/springboot/service/DBService.java
+++ src/main/java/com/api/springboot/service/DBService.java
@@ -21,13 +21,19 @@ public class DBService {
 	public void instanciaBaseDeDados() {
 
 		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-
+		Categoria cat2 = new Categoria(null, "Desenho", "Livros Infantís");
+		
 		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Loren ipsum", cat1);
-
+		Livro l2 = new Livro(null, "O mundo de Bob", "Autor desconhecido", "Loren ipsum", cat2);
+		
 		cat1.getLivros().addAll(Arrays.asList(l1));
+		cat2.getLivros().addAll(Arrays.asList(l2));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1));
 		livroRepository.saveAll(Arrays.asList(l1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat2));
+		livroRepository.saveAll(Arrays.asList(l2));
 
 	}
 
