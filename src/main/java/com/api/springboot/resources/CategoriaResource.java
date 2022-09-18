5ba59ee220c379650aa80b095c039c7df8135b3a
alessandroscoelho <alessandroscoelho@gmail.com>
2022-09-18 12:19:27 -0300

    criacao metodo delete para categoria

--- src/main/java/com/api/springboot/resources/CategoriaResource.java
+++ src/main/java/com/api/springboot/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,4 +51,15 @@ public class CategoriaResource {
 		Categoria newObj = categoriaService.update(id, objDTO);
 		return ResponseEntity.ok(new CategoriaDTO(newObj));
 	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		categoriaService.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	
+	
+	
 }
