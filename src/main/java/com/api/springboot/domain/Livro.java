d6e441a3da428c9082794c9ac8afa0328fc43ef7
alessandroscoelho <alessandroscoelho@gmail.com>
2022-09-19 15:22:31 -0300

    deploy para o Heroku

--- src/main/java/com/api/springboot/domain/Livro.java
+++ src/main/java/com/api/springboot/domain/Livro.java
@@ -16,6 +16,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Livro implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/api/springboot/resources/LivroResource.java
+++ src/main/java/com/api/springboot/resources/LivroResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +23,7 @@ import com.api.springboot.domain.Livro;
 import com.api.springboot.dtos.LivroDTO;
 import com.api.springboot.service.LivroService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
